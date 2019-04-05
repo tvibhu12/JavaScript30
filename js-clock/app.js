8e1adaf2563592be56d6406810eb193430247999
tvibhu12 <tvibhu12@gmail.com>
2019-04-05 21:57:59 +0530

    add minute and hour in clock

--- js-clock/app.js
+++ js-clock/app.js
@@ -4,11 +4,23 @@ var modal=
     //calculate Second and degree
    setTime:function(){
       const now = new Date();
+      console.log(now.getSeconds());
+      console.log(now.getMinutes());
+      console.log(now.getHours());
+      //get second
       const seconds= now.getSeconds();
       const secondsDegrees = ((seconds/60)*360)+90;
       //send degree to the octopuse.moveSecond()
     octopus.moveSecond(secondsDegrees);
 
+    //get minute
+    const minutes=now.getMinutes();
+    const minutesDegree=((minutes/60)*360)+90;
+    octopus.moveMinute(minutesDegree);
+    //get hour
+    const hours=now.getMinutes();
+    const hoursDegree=((minutes/12)*360)+90;
+    octopus.moveHours(hoursDegree);
    }
         
 };
@@ -23,6 +35,15 @@ var octopus=
 moveSecond:function(secondValue){
     //move Second's line in clock
 veiw.secondsHand(secondValue);
+},
+//move Mintue's line in clock
+moveMinute:function(minuteValue){
+ veiw.minuteHand(minuteValue);
+},
+//move Hour's line in clock
+moveHours:function(hourValue)
+{
+veiw.hourHand(hourValue);
 }
 };
 
@@ -35,8 +56,21 @@ var veiw=
         const secondHand=document.querySelector('.second-hand');
         //move Second's line in clock;
         secondHand.style.transform=`rotate(${secondValue}deg)`;
+    },
+    minuteHand:function(minuteValue)
+    {
+        const minuteHand=document.querySelector('.min-hand');
+        //move Minute's line in clock;
+        minuteHand.style.transform=`rotate(${minuteValue}deg)`;
+    },
+    hourHand:function(hourValue)
+    {
+        const hourHand=document.querySelector('.hour-hand');
+        //move Hour's line in clock;
+        hourHand.style.transform=`rotate(${hourValue}deg)`;
     }
 };
 
 //begining from here
+
 octopus.init();
