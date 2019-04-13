be54ca1df638b337af9ca233f2466fb8b765e7ca
tvibhu12 <tvibhu12@gmail.com>
2019-04-13 21:58:10 +0530

    complete task number 6

--- Array-Cardio-Practice/app.js
+++ Array-Cardio-Practice/app.js
@@ -50,7 +50,9 @@
       // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
       // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
   
-  
+  const category = document.querySelector('.mw-category');
+  const links = category.querySelectorAll('a');
+  const de = links.map(link => link.textContent).filter(streetName => streetName.includes('de'));
       // 7. sort Exercise
       // Sort the people alphabetically by last name
   
