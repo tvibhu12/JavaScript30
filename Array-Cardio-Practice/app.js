4f54965bd10f5ff2539ec6716ae98582023dff12
tvibhu12 <tvibhu12@gmail.com>
2019-04-14 17:01:17 +0530

    completed task number 8

--- Array-Cardio-Practice/app.js
+++ Array-Cardio-Practice/app.js
@@ -64,4 +64,14 @@ const peopleLast = people.sort((lastName,nextOne)=>{
 console.table(peopleLast);  
       // 8. Reduce Exercise
       // Sum up the instances of each of these
-    
+      const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+  const sum = data.reduce((obj,item)=>{
+      if(!obj[item])
+      {
+          obj[item]=0;
+
+      }
+      obj[item]++;
+      return obj;
+  },{});
+  console.log(sum);
