f462f12841c7e82aa55c110cedeb97811c83c462
tvibhu12 <tvibhu12@gmail.com>
2019-04-16 22:44:37 +0530

    final touchup

--- js-flexbox-image-gallery/app.js
+++ js-flexbox-image-gallery/app.js
@@ -1,8 +1,17 @@
 const panels = document.querySelectorAll('.panel');
 function toggle()
 {
-    console.log("hello");
+   
     this.classList.toggle('open');
+}
+function toggleText(e)
+{
+   
+    if(e.propertyName.includes('flex'))
+    {  
+      this.classList.toggle('open-active');
+    }
 }
 
 panels.forEach(panel=> panel.addEventListener('click',toggle));
+panels.forEach(panel=> panel.addEventListener('transitionend',toggleText));
