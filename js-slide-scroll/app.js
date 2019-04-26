2d54c3487ed35dbffa7d478f756a953ee0793539
tvibhu12 <tvibhu12@gmail.com>
2019-04-26 22:02:33 +0530

    add functionality in our project

--- js-slide-scroll/app.js
+++ js-slide-scroll/app.js
@@ -1,3 +1,27 @@
+const slideImgs=document.querySelectorAll('.slide-in');
+//DOM
+window.addEventListener('scroll',debounce(checkSlide));
+//hook up function
+function checkSlide(e)
+{
+  slideImgs.forEach(sliderImg=>
+    {
+        const slideInAt= (window.scrollY + window.innerHeight)-sliderImg.height/2;    //half of the iamge
+        const imgBottom= sliderImg.offsetTop+sliderImg.height;    //bottom of the image
+        const isHalfShown=slideInAt > sliderImg.offsetTop;     //is it at half of screen?
+        const isNotScrollPast= window.scrollY< imgBottom;
+        if(isHalfShown && isNotScrollPast)
+        {
+            sliderImg.classList.add('active');   //add .active class on image
+
+        }else
+        {
+            sliderImg.classList.remove('active');    //remove .active class on image
+            
+        }
+    });
+};
+//debounce function for count less scroll time
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
