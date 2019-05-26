201022c81871edbbd16059df4d7ffed0da5c3f0b
tvibhu12 <tvibhu12@gmail.com>
2019-05-26 23:26:21 +0530

    complete this project

--- js-Adding-Up-Times-with-Reduce/app.js
+++ js-Adding-Up-Times-with-Reduce/app.js
@@ -7,3 +7,12 @@ const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
       return (mins * 60) + secs;
     })
     .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
+
+    const mins = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+
+    console.log(hours, mins, secondsLeft);
